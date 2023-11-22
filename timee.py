7fc590930702a4589dfba76604618ad126943926
z3k9 <ajelelade@gmail.com>
2023-11-22 06:20:01 +0100

    chapter on time

--- timee.py
+++ timee.py
@@ -42,3 +42,36 @@ print(dt.year, dt.month, dt.day)
 print(dt.hour, dt.minute, dt.second)
 
 # a unix timestamp can be converted into a datetime object with the datetime.datetime.fromtimestamp()
+print(datetime.datetime.fromtimestamp(time.time()))
+
+# You can compare datetime objects with each other using comparison operators to find out which one precedes the other
+halloween_2019 = datetime.datetime(2019,10,31,0,0,0)
+christmas_2019 = datetime.datetime(2019,12, 31, 0,0,0)
+
+halloween_2019 > christmas_2019
+#False
+
+christmas_2019 != halloween_2019
+# True
+
+
+# timedelta datatype represents a duration of time rather than a moment in time
+delta = datetime.timedelta(days=11, hours=23, minutes=9, seconds=45)
+delta.days
+
+# The delta.total_seconds() will return the duration of the time in seconds alone
+delta.total_seconds()
+
+thousand_days = datetime.timedelta(days=1000)
+dt + thousand_days
+
+oct_21 = datetime.datetime(2019, 10, 21, 0,0,0)
+about_30_years = datetime.timedelta(days=365*30)
+
+oct_21 - about_30_years
+
+# Pausing until a specific date using while loop
+
+while datetime.datetime.now() < datetime.datetime(2023, 12,12):
+    time.sleep(1)
+    
